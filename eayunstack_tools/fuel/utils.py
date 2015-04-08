c8dfe86e78a6e301be6b5ae78cdd210ae0603f47
CoffeeChou <coffee.zyr@gmail.com>
2015-04-08 16:46:16 +0800

    add read_db() utils, read db and get backup list

--- eayunstack_tools/fuel/utils.py
+++ eayunstack_tools/fuel/utils.py
@@ -13,43 +13,59 @@ file_list = {}
 
 
 def backup_list():
-    backup_dirs = os.listdir(BACKUP_DIR + '/')
-    backup_dirs.sort(compare)
+    """List all the backup file"""
+    lines = read_db()
     i = 1
-    t = PrettyTable(['ID', 'Create Time', 'File Name'])
-    # when taking restore, there will be dirs named 'restore'. will not list 'restore' dir.
+    t = PrettyTable(['ID', 'Backup Time', 'Backup File'])
+    for line in lines:
+        # Delete the '\n' at the end of the line
+        line = line.strip('\n')
+        id = line.split(' ')[0]
+        backup_file = line.split(' ')[1]
+        file_split = backup_file.split('_', 4)
+        # Get the backup time from filename
+        c_date = file_split[2]
+        c_time = file_split[3].split('.', 1)[0][:2] \
+                 + ':' + \
+                 file_split[3].split('.', 1)[0][2:]
+        t.add_row([i, c_date + ' ' + c_time, backup_file])
+        i += 1
+    return t
+
+
+def read_db():
+    """Get Lines as List"""
+    if not os.path.exists('/tmp/tools.db'):
+        os.mknod('/tmp/tools.db')
+    db = open('/tmp/tools.db')
+    lines = db.readlines()
+    db.close()
+    return lines
+
+def backup_dict():
+    # Use sorted() method to sort by filename
+    backup_dirs = sorted(os.listdir(BACKUP_DIR + '/'))
     not_backup = 'restore'
     for backup_dir in backup_dirs:
         if not_backup in backup_dir:
             continue
         elif os.path.isfile(BACKUP_DIR + '/' + backup_dir):
-            file_split = backup_dir.split('_', 4)
-            c_date = file_split[2]
-            c_time = file_split[3].split('.', 1)[0][:2] + ':' + file_split[3].split('.', 1)[0][2:]
             backup_file = backup_dir
             dir_list[i] = ''
             file_list[i] = backup_file
         else:
-            file_split = backup_dir.split('_', 2)
-            c_date = file_split[1]
-            c_time = file_split[2][:2] + ':' + file_split[2][2:]
             backup_file = os.listdir(BACKUP_DIR + '/' + backup_dir + '/')
             dir_list[i] = backup_dir
             file_list[i] = backup_file[0]
-        # Put the result in a dictory, every sub-dir has only one backup file
-        t.add_row([i, c_date + ' ' + c_time, file_list[i]])
-        i += 1
-    return t
+    return file_list
 
+def write_db():
+    # append
+    db = open('/tmp/tools.db', 'a')
+    backup_dict()
+    id = file_list.keys()[-1]
+    backup_file = file_list[id]
+    db.writelines('%s' % id + ' ' + '%s\n' % backup_file)
+    db.close()
 
-# Sort the file, the file of most recent content modification will located at the end of the table
-def compare(x, y): 
-    stat_x = os.stat(BACKUP_DIR + '/' + x)
-    stat_y = os.stat(BACKUP_DIR + '/' + y)
-    if stat_x.st_mtime > stat_y.st_mtime:
-        return 1
-    elif stat_x.st_mtime < stat_y.st_mtime:
-        return -1
-    else:
-        return 0
 
