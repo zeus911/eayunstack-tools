29cb620df73c69164b069afa227103c1d61471d6
CoffeeChou <coffee.zyr@gmail.com>
2015-04-08 19:07:43 +0800

    move the restore_bck() method to restore.py, rewrite restore_bck()

--- eayunstack_tools/fuel/restore.py
+++ eayunstack_tools/fuel/restore.py
@@ -1,6 +1,8 @@
-# @file restore.py
 import logging
-from utils import backup_list
+import os
+import commands
+from utils import BACKUP_DIR
+from utils import read_db
 
 
 LOG = logging.getLogger(__name__)
@@ -8,6 +10,8 @@ LOG = logging.getLogger(__name__)
 def restore(parser):
     if parser.ID:
         restore_bck(parser.ID)
+    else:
+        parser.help
 
 def make(parser):
     '''Fuel Restore'''
@@ -24,13 +28,27 @@ def make(parser):
 def restore_bck(id):
     LOG.info('Starting Restore ...')
     LOG.info('It will take about 30 minutes, Please wait ...\n')
-    backup_list()
-    if id in file_list.keys():
-        backup_file = BACKUP_DIR + '/' + dir_list[id] + '/' + file_list[id]
-        (stat, out) = commands.getstatusoutput('dockerctl restore %s' % (backup_file))
+    backups = read_db()
+    backup_list = {}
+    backup_path = ''
+    for line in backups:
+        line = line.strip('\n')
+        # Put db info into a dictory
+        backup_list[line.split(' ')[0]] = line.split(' ')[1]
+    if id in backup_list.keys():
+        if os.path.isfile(BACKUP_DIR + '/' + backup_list[id]):
+            backup_path = BACKUP_DIR + '/' \
+                          + backup_list[id]
+        else:
+            backup_path = BACKUP_DIR + '/' \
+                          + backup_list[id].split('.', 1)[0].split('_', 1)[1] + '/' \
+                          + backup_list[id]
+        (stat, out) = commands.getstatusoutput('dockerctl restore %s' % backup_path)
         if stat != 0:
-            LOG.error('%s', out)            
+            LOG.error('%s', out)
         else:
             LOG.info('Restore successfully completed!\n')
+    else:
+        LOG.error('The ID does not exist, Please try again.\n')
 
 
